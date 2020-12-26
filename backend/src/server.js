32ad728327d2446cdf9ebe9d25349c333ccbe52c
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2020-12-26 17:56:13 -0300

    Conexão com o Banco de Dados.
    Instalado o Mongoose e configurada a URL para conexão com o Banco de Dados "omnistack9" no MongoDB;
    Configurados dois parâmetros específicos dentro do "mongoose.connect" para utilizar o novo padrão de URL conforme instruído no tutorial.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://omnistack_dev:KcAtSiNm0O@omnistack9.pwlbv.mongodb.net/omnistack9?retryWrites=true&w=majority', {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+});
 
 //req.query = Para acessar as Query Params (para filtros nas requisições);
 //req.params = Para acessar as Route Params (para edição e deleção nas requisições);
