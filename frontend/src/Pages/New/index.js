d144f4bc4e6996f6f8c3576d3b4c77e12d394882
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2020-12-27 17:55:50 -0300

    Inicialização da Tela - New.
    Implementada a Estrutura HTML e Estilização CSS Inicial junto do meu CSS Próprio da Tela New de cadastro dos Spots;
    Implementadas verificações e funcionalidades para o Campo de Upload e atualização do Thumbnail do Spot.

--- frontend/src/Pages/New/index.js
+++ frontend/src/Pages/New/index.js
@@ -1,7 +1,63 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+
+import camera from '../../assets/camera.svg';
+
+import './styles.css';
 
     export default function New() {
 
-        return <h1> New </h1>
+        const [thumbnail, setThumbnail] = useState(null);
+
+        const [company, setCompany] = useState('');
+
+        const [techs, setTechs] = useState('');
+
+        const [price, setPrice] = useState('');
+
+        const preview = useMemo(
+
+            () => {
+
+                return thumbnail ? URL.createObjectURL(thumbnail) : null;
+
+            }, [ thumbnail ]
+
+        );
+
+            function handleSubmit() {
+
+
+
+            }
+
+                return (
+
+                    <form onSubmit={ handleSubmit }>
+
+                        <label id="thumbnail" style={ { backgroundImage: `url( ${ preview } )` } } className={ thumbnail ? 'has-thumbnail' : '' }>
+
+                            <input type="file" onChange={ event => setThumbnail(event.target.files[0]) }/>
+
+                                <img src={ camera } alt="Select Spot Image"/>
+
+                        </label>
+
+                        <label htmlFor="company"> EMPRESA <span> * </span> </label>
+
+                            <input id="company" placeholder="O nome completo da sua empresa" value={ company } onChange={ event => setCompany(event.target.value) }/>
+
+                                <label htmlFor="techs"> TECNOLOGIAS <span> * </span> <span> (separadas por vírgula) </span> </label>
+
+                                <input id="techs" placeholder="Tecnologias utilizadas pela sua empresa" value={ techs } onChange={ event => setTechs(event.target.value) }/>
+
+                                    <label htmlFor="techs"> VALOR DA DIÁRIA <span> * </span> <span> (em branco para gratuita) </span> </label>
+
+                                    <input id="price" placeholder="Valor desejado para a diária" value={ price } onChange={ event => setPrice(event.target.value) }/>
+
+                                        <button type="submit" className="btn"> Cadastrar Spot </button>
+
+                    </form>
+
+                )
 
     }
