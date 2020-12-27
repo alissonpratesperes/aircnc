eced5a30a06a9a7fed4b2b2b41547decd16632c7
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2020-12-27 06:21:01 -0300

    Estrutura HTML inicial da Página de Login.
    Criada e ajustada a Estrutura HTML inicial da Página de Login da aplicação.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,37 @@
+import React from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import logo from './assets/logo.svg';
 
-export default App;
+  function App() {
+
+    return (
+
+      <div className="container">
+
+        <img src={logo} alt="Air CnC"/>
+
+          <div className="content">
+
+            <p> Ofereça <strong> spots </strong> para programadores e encontre <strong> talentos </strong> para sua empresa. </p>
+
+              <form>
+
+                <label htmlFor="email"> E-MAIL * </label>
+
+                  <input type="email" id="email" placeholder="Seu melhor e-mail"/>
+
+                    <button type="submit"> Entrar </button>
+
+              </form>
+
+          </div>
+
+      </div>
+
+    );
+
+  }
+
+    export default App;
