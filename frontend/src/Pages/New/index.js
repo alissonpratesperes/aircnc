c1d89442ed93ffa3db6927504af05117255bcb5d
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2020-12-27 19:57:55 -0300

    Implementadea a Conexão da Página New com o BACKEND.
    Tivemos que lidar de forma diferente do Login com o POST da Página New de Cadastro de Spots, uma vez que utiliza o POST via Multi-Part por causa da Imagem e não em JSON conforme o Login;
    Implementado o recurso "History" também para a navegação do usuário após o Cadastro do Spot, conforme feito anteriormente na Página de Login.

--- frontend/src/Pages/New/index.js
+++ frontend/src/Pages/New/index.js
@@ -1,10 +1,12 @@
 import React, { useState, useMemo } from 'react';
 
+import api from '../../services/api';
+
 import camera from '../../assets/camera.svg';
 
 import './styles.css';
 
-    export default function New() {
+    export default function New( { history } ) {
 
         const [thumbnail, setThumbnail] = useState(null);
 
@@ -24,9 +26,26 @@ import './styles.css';
 
         );
 
-            function handleSubmit() {
+            async function handleSubmit(event) {
 
+                event.preventDefault();
 
+                    const data = new FormData();
+
+                    const user_id = localStorage.getItem('user');
+
+                        data.append('thumbnail', thumbnail);
+                        data.append('company', company);
+                        data.append('price', price);
+                        data.append('techs', techs);
+
+                            await api.post('/spots', data, {
+
+                                headers: { user_id }
+
+                            })
+
+                                history.push('/dashboard');
 
             }
 
