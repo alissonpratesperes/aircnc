8b79e6892082b122a4c3322e434d688546c6110d
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2020-12-30 15:03:58 -0300

    Implementação inicial - Tela de Login.
    Implementada inicialmente a tela de Login com o Formulário de Login e já com as estilizações iniciais e próprias
    Automatizado o comportamento do Form de Login para quando o Teclado abrir para digitação não ficar sobre, através da verificação de plataforma que está rodando a aplicação.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
     const api = axios.create ({
 
-    baseURL: 'http://localhost:3333',
+        baseURL: 'http://localhost:3333',
 
-    });
+        });
 
-        export default api;
+            export default api;
--- mobile/src/pages/Login.js
+++ mobile/src/pages/Login.js
@@ -1,20 +1,103 @@
 import React from 'react';
 
-import { View, Text} from 'react-native';
+import { View, KeyboardAvoidingView, Platform, Image, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+import logo from '../assets/logo.png';
 
     function Login() {
 
         return (
 
-            <View>
+            <KeyboardAvoidingView enabled={ Platform.OS === 'ios' } behavior="padding" style={ styles.container } >
 
-                    <Text> Hello World </Text>
+                    <Image source ={ logo } />
 
-            </View>
+                        <View style={ styles.form } >
+
+                            <Text style={ styles.label } > SEU E-MAIL <Text style={ styles.span } >*</Text> </Text>
+
+                                <TextInput style={ styles.input } placeholder="Seu melhor e-mail" keyboardType="email-address" autoCapitalize="none" autoCorrect={ false } />
+
+                                    <Text style={ styles.label } > TECNOLOGIAS <Text style={ styles.span } >*</Text> </Text>
+
+                                        <TextInput style={ styles.input } placeholder="Tecnologias de interesse" autoCapitalize="words" autoCorrect={ false } />
+
+                                            <TouchableOpacity style={ styles.button } >
+
+                                                <Text style={ styles.buttonText } > ENCONTRAR SPOTS </Text>
+
+                                            </TouchableOpacity>
+
+                        </View>
+
+            </KeyboardAvoidingView>
 
         );
 
     }
+
+    const styles = StyleSheet.create({
+
+        container: {
+
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+
+        },
+
+        label: {
+
+            fontWeight: 'bold',
+            color: '#444444',
+            marginBottom: 8,
+
+        },
+
+        span: {
+
+            color: '#E14F50',
+
+        },
+
+        form: {
+
+            alignSelf: 'stretch',
+            paddingHorizontal: 30,
+
+        },
+
+        input: {
+
+            borderWidth: 1,
+            borderColor: '#DDDDDD',
+            paddingHorizontal: 20,
+            fontSize: 16,
+            height: 44,
+            marginBottom: 20,
+            borderRadius: 10,
+
+        },
+
+        button: {
+
+            height: 42,
+            backgroundColor: '#F05A5B',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 10
+
+        },
+
+        buttonText: {
+
+            color: '#FFFFFF',
+            fontWeight: 'bold',
+            fontSize: 16
+
+        },
+
+    });
 
 
         export default Login;
