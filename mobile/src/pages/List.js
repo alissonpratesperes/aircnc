dd13f3d644135107fa88e2986c6c85eed23ae3ea
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2020-12-30 14:22:20 -0300

    Retomada de desenvolvimento do App Mobile.
    Recriado o Diretório "mobile" novamente da forma correta com o "expo init mobile", após ter instalado o expo de forma glogal na máquina (com o comando "yarn add global expo-cli");
    Removido a pasta oculta ".git" que já se tinha conhecimento que esta poderia bugar o ".git" principal do projeto no root "aircnc" pois o expo adiciona automaticamente;
    Descoberto o problema que estava impedindo o desenvolvimento do App Mobile (e consequentemente o andamento das aulas 4 e 5), que se tratava da Sintaxe do React Native (a sintaxe utilizada na Week 9 não funcionou, porém a sintaxe da Week 10 funcionou) - utilizando todos os pacotes padrão sem adicionar nenhum específico (apenas instalando os utilizados na Week 9 devido a recriação desse diretório), conforme imagens em: "./_others/Imagens - Erro ao inicializar Roteamento".

---
+++ mobile/src/pages/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { View } from 'react-native';
+
+    function List() {
+
+        return <View />
+
+    }
+
+        export default List;
---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { View, Text} from 'react-native';
+
+    function Login() {
+
+        return (
+
+            <View>
+
+                    <Text> Hello World </Text>
+
+            </View>
+
+        );
+
+    }
+
+
+        export default Login;
