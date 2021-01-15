1f254996a3caa51d421a5f035d80257249c1b413
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-15 09:59:26 -0300

    Deleção do MOBILE e aplicação dos novos arquivos.
    Deletada a pasta "node_modules" do diretório MOBILE;
    Aplicados os novos arquivos (substituindo os antigos bugados) do MOBILE que foram refatorados, e tiveram os bugs encontrados durante o desenvolvimento, corrigidos.

--- mobile/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-    const api = axios.create ({
-
-        baseURL: 'http://192.168.0.101:3333', //Esse endereço é pego de dentro da Seção "Connection" do Expo, precisa ser alterado manualmente para que funcione de acordo.
-
-    });
-
-        export default api;
