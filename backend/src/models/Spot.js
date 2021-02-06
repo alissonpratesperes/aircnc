a7cdd12e2141ff06de4084dc93143e812cea803b
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-02-06 17:01:35 -0300

    Testes da Aplicação.
    Realizados alguns testes de ciclo de uso da Aplicação, tudo em ordem.

--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -12,7 +12,7 @@ const SpotSchema = new mongoose.Schema({
 });
 
     SpotSchema.virtual('thumbnail_url').get(function() {
-        return `http://192.168.0.101:3333/files/${this.thumbnail}`
+        return `http://192.168.0.102:3333/files/${this.thumbnail}`
     });
 
         module.exports = mongoose.model('Spot', SpotSchema);
